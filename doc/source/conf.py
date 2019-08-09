0b73b8ec638f0e608ffb29e2a30978516421ed74
Dragan Zuvic <git.zuvic@posteo.de>
2019-08-09 21:28:12 +0200

    [chore] Update dependencies

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -49,7 +49,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'jtsgen'
-copyright = '2017, Dragan Zuvic'
+copyright = '2017-2019, Dragan Zuvic'
 author = 'Dragan Zuvic'
 
 # The version info for the project you're documenting, acts as replacement for
